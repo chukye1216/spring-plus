184cdb9f1dd9cf6a9e4e3f7bda88d3ec2d55ba12
chukye1216 <chukye1216@gmail.com>
2024-10-11 00:43:53 +0900

    Level. 2 8. QueryDSL

---
+++ src/main/java/org/example/expert/domain/todo/repository/TodoRepositoryImpl.java
@@ -0,0 +1,26 @@
+package org.example.expert.domain.todo.repository;
+
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import lombok.RequiredArgsConstructor;
+import org.example.expert.domain.todo.entity.Todo;
+
+import java.util.Optional;
+
+@RequiredArgsConstructor
+public class TodoRepositoryImpl implements TodoRepositoryCustom {
+
+    private final JPAQueryFactory queryFactory;
+
+    @Override
+    public Optional<Todo> findByIdWithUser(Long todoId) {
+        QTodo todo = QTodo.todo;
+        QUser user = QUser.user;
+
+        return Optional.ofNullable(
+                queryFactory.selectFrom(todo)
+                        .leftJoin(todo.user, user).fetchJoin()
+                        .where(todo.id.eq(todoId))
+                        .fetchOne()
+        );
+    }
+}
